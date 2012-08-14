4ced05b1e5c928f27ad3bd3f7e00196a69368c8d
Patrik Lundin <patrik.lundin.swe@gmail.com>
2012-08-14 12:14:44 +0200

    Initial commit of general idea.

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Script that populates the home directory with available files.
+# OS-specific files will have precedence.
+#
+
+BASEDIR=$(dirname $0)
+OS=$(uname -s)
+RSYNC='rsync -av'
+
+# Make sure we can run the script from wherever.
+cd ${BASEDIR}
+
+# We want the git repo to be fresh.
+git pull
+
+# Sync default files.
+${RSYNC} ./default/ ${HOME}/
+
+# Sync any OS-specific files if they exist.
+if [ -d ./${OS} ]; then
+    ${RSYNC} ./${OS}/ ${HOME}
+fi
