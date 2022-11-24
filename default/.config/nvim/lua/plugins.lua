7af31e26836fe9443da7c426450ded30c70c0253
Patrik Lundin <patrik@sigterm.se>
2022-11-24 08:23:32 +0100

    Add git plugins
    Also add vim-devicons last as requested by the comment

--- default/.config/nvim/lua/plugins.lua
+++ default/.config/nvim/lua/plugins.lua
@@ -5,11 +5,13 @@ require('packer').startup(function()
   use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
   use 'preservim/nerdtree' -- File browser window
   use 'gruvbox-community/gruvbox' -- Colortheme
-  use 'ryanoasis/vim-devicons' -- Always load the vim-devicons as the very last one.
   -- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { {'nvim-lua/plenary.nvim'} } }
+  use 'tpope/vim-fugitive' -- git integration
+  use 'airblade/vim-gitgutter' -- git integration
+  use 'ryanoasis/vim-devicons' -- Always load the vim-devicons as the very last one.
 end)
