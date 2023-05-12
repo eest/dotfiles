80663e5b3ad72ccf186350da8fbfa2681fdb84f2
Patrik Lundin <patlu@sunet.se>
2023-05-12 12:14:05 +0200

    Remove nerdtree and devicons
    No longer used.

--- default/.config/nvim/lua/autocmd.lua
+++ default/.config/nvim/lua/autocmd.lua
@@ -13,22 +13,4 @@ if vim.fn.has('nvim-0.7.0') == 1 then
         "FileType",
         { pattern = {"mail"} , command = "set textwidth=72" }
     )
-
-    --[[
-    -- Make NERDTree open automatically on startup
-    vim.api.nvim_create_autocmd({"VimEnter"}, {
-        command = "NERDTree"
-    })
-
-    -- Place cursor in the main code window instead of the NERDTree
-    -- window
-    vim.api.nvim_create_autocmd({"VimEnter"}, {
-        command = "wincmd p"
-    })
-    --]]
-
-    -- Close vim if the only window left is NERDTree
-    vim.api.nvim_create_autocmd({"BufEnter"}, {
-        command =  [[ if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif ]]
-    })
 end
--- default/.config/nvim/lua/plugins.lua
+++ default/.config/nvim/lua/plugins.lua
@@ -3,7 +3,6 @@ local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
   use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-  use 'preservim/nerdtree' -- File browser window
   use 'gruvbox-community/gruvbox' -- Colortheme
   -- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
@@ -13,5 +12,4 @@ require('packer').startup(function()
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { {'nvim-lua/plenary.nvim'} } }
   use 'tpope/vim-fugitive' -- git integration
   use 'airblade/vim-gitgutter' -- git integration
-  use 'ryanoasis/vim-devicons' -- Always load the vim-devicons as the very last one.
 end)
