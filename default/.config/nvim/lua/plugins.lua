3a98922b78998c3582bf2fefe909f2eaefb19a60
Patrik Lundin <patlu@sunet.se>
2024-09-03 12:40:24 +0200

    Update nvim-telescope
    Fixes "Invalid 'event': 'User TelescopeFindPre'" error when trying to use
    it.

--- default/.config/nvim/lua/plugins.lua
+++ default/.config/nvim/lua/plugins.lua
@@ -9,7 +9,7 @@ require('packer').startup(function()
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
-  use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { {'nvim-lua/plenary.nvim'} } }
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { {'nvim-lua/plenary.nvim'} } }
   use 'tpope/vim-fugitive' -- git integration
   use 'airblade/vim-gitgutter' -- git integration
 end)
